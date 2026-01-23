f6cef629be76aa9c0aa7dceba9667748d02ae05a
Anita592 <ao69964@ubt-uni.net>
2026-01-23 15:40:50 +0100

    added  the review category

--- Models/ReviewCategory.cs
+++ Models/ReviewCategory.cs
@@ -1,6 +1,15 @@
-﻿namespace HotelMgm.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelMgm.Models
 {
     public class ReviewCategory
     {
+        [Key]
+        public int ReviewCategoryID { get; set; }
+
+        [Required]
+        public string CategoryName { get; set; }
+
+        public ICollection<Review> Reviews { get; set; }
     }
 }
