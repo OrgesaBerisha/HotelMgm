06122f132aa8ae888649549d94d77eac3ab18fa7
festathaqi <festathaqii@gmail.com>
2026-01-25 18:52:36 +0100

    added to admin dashboard the service admin

--- obj/Debug/net8.0/HotelMgm.AssemblyInfo.cs
+++ obj/Debug/net8.0/HotelMgm.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelMgm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d53f8938db9c716bb66d1458c25d55e9bb76e8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9bbfa6a9388c0f9993f898ee4d3737bc3e91acf")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelMgm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelMgm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
