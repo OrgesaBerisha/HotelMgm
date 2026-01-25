9d53f8938db9c716bb66d1458c25d55e9bb76e8a
festathaqi <festathaqii@gmail.com>
2026-01-25 18:48:41 +0100

    started on services dashboards

--- obj/Debug/net8.0/HotelMgm.AssemblyInfo.cs
+++ obj/Debug/net8.0/HotelMgm.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelMgm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+357c64a1839a065366cac53b9522ca365b3ba949")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20256e0f0201fd3da6b295b020de402e0f934f98")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelMgm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelMgm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
